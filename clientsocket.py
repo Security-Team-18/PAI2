9af7561c021df031c131ab1435b9f57907c9e0d2
alefrarod <alefrarod@alum.us.es>
2022-03-14 17:42:21 +0100

    mejora clientsocket.py

--- clientsocket.py
+++ clientsocket.py
@@ -1,13 +1,19 @@
 # clientsocket.py
 
+import hashlib
 import socket
+import hmac
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 3030  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
+    s.connect((HOST, PORT))   
+    print("Introduzca la transacción con este formato:")
+    valor=input("Cuenta origen, cuenta destino, cantidad\n")
+    maker=hmac.new(b"52772321", bytes(valor,"utf-8"), hashlib.sha256)
+    digest=maker.hexdigest()
+    s.sendall(bytes(digest,"utf-8"))
     data = s.recv(1024)
 
 print(f"Received {data!r}")
