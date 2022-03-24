e705c680fa7844a51b623fe9749cecceedd32bb2
Benjamin Crespo <72440017+bencrealc@users.noreply.github.com>
2022-03-24 17:41:53 +0100

    Delete log.py

--- log.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def log(transaccion, valor):
-    
-    now = datetime.now()
-    fecha=now.strftime("%d/%m/%Y %H:%M:%S")
-    with open("./almacenamiento/log.txt", 'a') as file:
-        file.writelines(fecha+ " -> Transacción " + transaccion + " no íntegra \n")
